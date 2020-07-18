79237fda99bd4a495c394c8fd30cd579eee083d2
amkera <60901491+amkera@users.noreply.github.com>
2020-07-17 23:15:39 -0700

    added failure method to omniauth controller
    added failure method to omniauth controller

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -11,4 +11,9 @@ class OmniauthController < ApplicationController
     end
   end
 
+  def failure
+    flash[:error] = "There was a problem signing in through Github. Please register or try again."
+    redirect_to new_esthetician_registration_url
+  end 
+
 end
