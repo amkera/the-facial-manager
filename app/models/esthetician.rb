32fa33c2c2a2c58b6cb9f2dc95198b9b5f6e5a55
amkera <60901491+amkera@users.noreply.github.com>
2020-07-17 23:08:16 -0700

    self.create_from_provider_data(provider_data)
    added self.create_from_provider_data(provider_data) to est model

--- app/models/esthetician.rb
+++ app/models/esthetician.rb
@@ -2,5 +2,15 @@ class Esthetician < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:github]
+
+  def self.create_from_provider_data(provider_data)
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |esthetician|
+      #fetch user if already in db, or create one
+      esthetician.email = provider_data.info.email
+      esthetician.password = Devise.friendly_token(0, 20)
+    end 
+  end
+
 end
