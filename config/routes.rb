fb9ef69645528635a7e8496fa4a91d71914f154a
amkera <60901491+amkera@users.noreply.github.com>
2020-07-17 23:56:05 -0700

    last commit for now
    created est routes and an index for now

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :clients
   resources :facials
+  resources :estheticians
 
   root 'clients#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
