37691d2f31e8bed03570167e2e30e02e23558e5d
amkera <60901491+amkera@users.noreply.github.com>
2020-07-19 16:07:28 -0700

    changed root route to welcome page
    changed root route to welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :facials
   resources :estheticians
 
-  root 'clients#index'
+  root "application#welcome"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
