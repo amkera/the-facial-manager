a01842f1c5a1f6680b8f376a9e3b96f7761530ad
amkera <60901491+amkera@users.noreply.github.com>
2020-07-18 21:08:01 -0700

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#facial1 = Facial.new(facial_date: 5.days.ago, facial_time: 5.seconds.ago, acne: true, cystic_acne: true, deep_pitted_scars: true, dryness: true, dullness: true, flakiness: true, flat_dark_scars: true, redness: true, wrinkles: true, other: "Other other other", client_id: 1, esthetician_id: 1)
