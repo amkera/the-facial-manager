e422f30da39093c7b2a4ebb404c8a646ce891c9b
amkera <60901491+amkera@users.noreply.github.com>
2020-07-18 19:06:48 -0700

    coded out the show page
    coded out the show page

--- app/controllers/facials_controller.rb
+++ app/controllers/facials_controller.rb
@@ -1,11 +1,12 @@
 class FacialsController < ApplicationController
+  def show
+    @facial = Facial.find(params[:id])
+  end
+
   def index
     @facials = Facial.all
   end
 
-  def show
-    find_facial
-  end
 
   def new
     @facial = Facial.new
@@ -14,16 +15,15 @@ class FacialsController < ApplicationController
   def create
     @facial = Facial.new(facial_params)
     @facial.save
-    #byebug
     redirect_to facial_path(@facial)
   end
 
   def edit
-    find_facial
+    @facial = Facial.find(params[:id])
   end
 
   def update
-    find_facial
+    @facial = Facial.find(params[:id])
     @facial.update(facial_params)
     redirect_to facial_path(@facial)
   end
@@ -33,10 +33,6 @@ class FacialsController < ApplicationController
 
   private
 
-  def find_facial
-    @facial = Facial.find_by(id: params[:id])
-    #facial.find_by(id: params[:id])
-  end
 
   def facial_params
     params.require(:facial).permit(:facial_date, :facial_time, :acne, :cystic_acne, :deep_pitted_scars, :dryness, :dullness, :flakiness, :flat_dark_scars, :redness, :wrinkles, :other, :client_id, :esthetician_id)
