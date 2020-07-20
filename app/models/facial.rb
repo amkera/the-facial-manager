32139198055b9e031469ec18d0a4559f8c416581
amkera <60901491+amkera@users.noreply.github.com>
2020-07-20 11:07:48 -0700

    Adding validations to facial model

--- app/models/facial.rb
+++ app/models/facial.rb
@@ -1,4 +1,13 @@
 class Facial < ApplicationRecord
   belongs_to :client
   belongs_to :esthetician
+
+  validates :facial_date, presence: true
+  validates :facial_time, presence: true
+
+  validates :client_id, presence: true
+  validates :esthetician_id, presence: true
+
+  
+
 end
