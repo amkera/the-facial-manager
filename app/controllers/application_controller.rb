71601d23e07e6326a8787e9a797fe75fa7894832
amkera <60901491+amkera@users.noreply.github.com>
2020-07-25 00:34:43 -0700

    beginning to prep for styling
    beginning to prep for styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
 
   def welcome
-    redirect_to new_esthetician_session_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,12 @@ Rails.application.routes.draw do
     resources :facials, only: [:show, :index, :new, :edit]
   end
 
-
   #Gives access to /estheticians/:esthetician_id/facials/new,
   #and a new_esthetician_facial_path helper.
 
   resources :facials
 
   root "application#welcome"
+
 
 end
