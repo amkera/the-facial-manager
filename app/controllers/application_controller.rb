541c5d995374479a0c115035b40cbd1123a8437e
amkera <60901491+amkera@users.noreply.github.com>
2020-07-25 21:47:52 -0700

    added def set_esthetician to application controller
    to give access to the views for @esthetician because they didn't have access before

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_esthetician
 
   def welcome
   end
 
   private
-  
+
   def set_esthetician
     @esthetician = current_esthetician
   end
