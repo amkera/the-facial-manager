85c3323dd99ea46d2c5740bee4c48efe093ef6fa
amkera <60901491+amkera@users.noreply.github.com>
2020-07-31 10:27:51 -0700

    minor error in devise functionality
    email was not being carried in omniauth info so Z helped me add the uid as an email

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -4,11 +4,10 @@ class OmniauthController < ApplicationController
     @esthetician = Esthetician.create_from_provider_data(request.env['omniauth.auth'])
     if @esthetician.persisted?
       sign_in_and_redirect @esthetician
-      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
+      #set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
     else
       flash[:error] = "There was a problem signing in through Github. Please register or try again."
-      #redirect_to new_esthetician_registration_url
-      redirect_to clients_url
+      redirect_to new_esthetician_registration_url
     end
   end
 
--- app/models/esthetician.rb
+++ app/models/esthetician.rb
@@ -10,7 +10,7 @@ class Esthetician < ApplicationRecord
   def self.create_from_provider_data(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |esthetician|
       #fetch user if already in db, or create one
-      esthetician.email = provider_data.info.email
+      esthetician.email = (provider_data.info.email.nil?) ? "#{provider_data.uid}@email.com" : provider_data.info.email
       esthetician.password = Devise.friendly_token[0, 20]
     end
   end
